c92885879df1409e8b7733cdca04559104f8534b
Claire Bourdon <cebourdon625@gmail.com>
2023-07-23 14:09:55 -0400

    added comment

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI) //Add your database credentials here!
     .then(() => console.log('Connected to DB!'))
     .catch(error => console.log(error.message));
 
